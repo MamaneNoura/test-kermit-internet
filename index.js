d84dd7d4c3e751e4d680deb80f07f2ba29530f42
dzn1636 <tuandung.hoang0228@gmail.com>
2019-05-14 17:58:02 +0200

    add

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.get('/', (req, res) => {
+  res.json({ foo: 'bar' });
+});
 
 app.listen(port, ip, err => {
   if (err) throw err;
